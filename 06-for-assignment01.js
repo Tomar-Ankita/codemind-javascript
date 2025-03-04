7dd89579b38c283c07ea7099001803fd74f4c060
Tomar-Ankita <ankitatomar81299@gmail.com>
2025-03-04 16:48:46 +0530

    06-for-assignment01.js
    for assignment

---
+++ 06-for-assignment01.js
@@ -0,0 +1,59 @@
+// Print numbers from 5-15//
+console.log(" Print numbers from 5-15:");
+
+var output =[];
+for (var index = 5; index < 16; index++) {
+   
+   output.push(index);
+    
+    
+}
+console.log(output.join(" "));
+console.log("----------------------------");
+
+console.log(" Print numbers from 50-40:")
+var output = [];
+for (var index = 50; index >=40; index--) {
+    output.push(index);
+    
+}
+console.log(output.join(" "));
+console.log("--------------------------------");
+console.log("Print first 15 odd numbers");
+
+var output = [];
+for (var index = 1; index < 30; index= index+2) {
+    output.push(index);
+    
+}
+console.log(output.join(" "));
+console.log("------------------------------------");
+console.log("Print table of 5");
+var output = [];
+for (var index = 5; index <= 50; index= index+5) {
+    output.push(index);
+    
+}
+console.log(output.join(" "));
+console.log("-------------------------------------");
+console.log("Print the table of 10");
+var output=[];
+for (var index = 10; index <=100; index=index+10) {
+    output.push(index);
+    
+}
+console.log(output.join(" "));
+console.log("-------------------------------------");
+console.log("Print the table of 10 in reverse order");
+var output=[];
+for (var index = 100; index >=10; index=index-10) {
+    output.push(index);
+    
+}
+console.log(output.join(" "));
+console.log("----------------------------------");
+
+
+
+
+
