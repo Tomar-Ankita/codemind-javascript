e07d9d9d7dca517f2f76af7f6130dfb496b68e67
anjalitomar127 <anjalitomarabh@gmail.com>
2025-03-04 14:36:13 +0530

    Create 02-typeOf.js
    02-tyoeOf

---
+++ 02-typeOf.js
@@ -0,0 +1,25 @@
+var bankName;
+console.log(`Value before initialization:${bankName}`);
+ bankName ="SBI Bank"
+console.log(`Value after initialization:${bankName}`);
+console.log(`Datatype:${typeof(bankName)}`);
+var marks;
+marks = '90%'
+console.log(`Marks:${marks},   datatype:${typeof(marks)}`);
+var isWorking;
+console.log(`Before initialization:${isWorking}`);
+isWorking = true;
+console.log(`After initialization:${isWorking}, datatype:${typeof(isWorking)}`);
+var totalCount;
+totalCount = "Hundred and Seven";
+console.log(`Total count value:${totalCount},  datatype:${typeof(totalCount)}`);
+totalCount = 107;
+console.log(`Total count is:${totalCount},  datatype:${typeof(totalCount)}`);
+
+
+
+
+
+
+
+
