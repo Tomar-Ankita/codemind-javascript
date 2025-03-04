8d704e583e69b9fbd64a133e1049580a5befd997
anjalitomar127 <anjalitomarabh@gmail.com>
2025-03-04 14:31:14 +0530

    01-variable Assignment.js
    01-variable Assignment

---
+++ 01-variableAssign.js
@@ -0,0 +1,40 @@
+var myName;
+console.log("Before initialization : ", myName);
+var myName;
+myName="Ankita Tomar";
+console.log("My full name is:",myName);
+var myFullName;
+myFullName = "Ankita Akhilesh Tomar";
+console.log("My Full Name:",myFullName); 
+var muLove;
+myLove = "I love only JS"
+console.log("My love:",myLove);
+var firstName;
+firstName = "Ankita"
+console.log("First name:",firstName);
+var lastName;
+lastName = "Tomar"
+console.log("Last Name:",lastName);
+var age;
+age = "24";
+console.log("Age:",age);
+var address;
+address = "Pandavnagar";
+console.log("Address:",address);
+var pinCode;
+pinCode = 152116;
+console.log("PinCode:",pinCode);
+var gender;
+gender = "Female";
+console.log("Gender:",gender);
+var isMarried;
+isMarried = "No"
+console.log("isMarried:",isMarried);
+
+
+
+
+
+
+
+
