a6645321a9714b3aed33864cdf6b406df1c0c2a7
Tomar-Ankita <ankitatomar81299@gmail.com>
2025-03-12 20:15:14 +0530

    27_domAssign.html,27_domScript.js
    27_domAssign.html,27_domScript.js

---
+++ 27_domScript.js
@@ -0,0 +1,7 @@
+const mainHeading=document.querySelector("h1");
+console.log(mainHeading.innerText);
+const secondHeading=document.querySelector("#avail");
+console.log(secondHeading.innerText);
+const advDiv=document.querySelector("#advantages")
+console.log(advDiv.innerText);
+
